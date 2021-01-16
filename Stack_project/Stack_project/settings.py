fd03a08a5be19eeb3d983f47da2beee29d510309
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-16 13:55:26 +0900

    registered crispy forms and Stack

--- Stack_project/Stack_project/settings.py
+++ Stack_project/Stack_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Stack.apps.StackConfig",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
