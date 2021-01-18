3f2dd41bbcb556325eae2028b0c19c64c95701de
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-18 19:51:52 +0900

    prepped view for form

--- Stack_project/Stack/views.py
+++ Stack_project/Stack/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import StockForm
 
 
 def index(request):
+    # if request.method =='POST':
+    #     form = StockForm(request.POST)
+    #     if form.is_valid():
+    #         return 
     return render(request, "Stack/index.html")
