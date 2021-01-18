e89e20c84fca5198558d2a8dd0417951f5fb9a8f
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-18 19:51:37 +0900

    Started first model

--- Stack_project/Stack/models.py
+++ Stack_project/Stack/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class StockInformation(models.Model):
+    name = models.CharField(max_length=30)
+    ticker = models.CharField(max_length=10)
