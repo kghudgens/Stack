1081882ac0a45e50a5c945edde5b59b8d23f6fb2
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-23 23:40:11 +0900

    created new form for to do app

--- Stack_project/Stack/forms.py
+++ Stack_project/Stack/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class StockForm(forms.Form):
-    ticker = forms.CharField(label="Enter Ticker", max_length=10)
+    name = forms.CharField(label="Enter the name of the task: ")
+    detail = forms.CharField(
+        label="Enter the details of your task:", widget=forms.Textarea
+    )
+    date_created = forms.DateTimeField()
