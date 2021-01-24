553a694dd3fb506924996b494a290e12b7c5b213
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-24 15:15:36 +0900

    edited field

--- Stack_project/Stack/forms.py
+++ Stack_project/Stack/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 
 
-class StockForm(forms.Form):
+class TaskForm(forms.Form):
     name = forms.CharField(label="Enter the name of the task: ")
     detail = forms.CharField(
-        label="Enter the details of your task:", widget=forms.Textarea
+        label="Enter the details of your task: ", widget=forms.Textarea
     )
-    date_created = forms.DateTimeField()
