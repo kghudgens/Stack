e8d46898b9d8b6eed0c8037dd02119a5b8877cf5
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-24 14:49:58 +0900

    edited detail field

--- Stack_project/Stack/models.py
+++ Stack_project/Stack/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class TaskManager(models.Model):
     task_name = models.CharField(max_length=30)
-    detail = models.CharField(max_length=10)
+    detail = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
