ce932dbf9d63f32123c8dd5405973c6018926522
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-24 15:15:18 +0900

    put form in view

--- Stack_project/Stack/views.py
+++ Stack_project/Stack/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponseRedirect
-from .forms import StockForm
+from .forms import TaskForm
 
 
 def index(request):
-    # if request.method =='POST':
-    #     form = StockForm(request.POST)
-    #     if form.is_valid():
-    #         return 
-    return render(request, "Stack/index.html")
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Your task has been added")
+            return HttpResponseRedirect("/index/")
+    else:
+        form = TaskForm()
+    return render(request, "Stack/index.html", {"form": form})
