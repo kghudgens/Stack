77ee8aafa036e4177905184bb779b4484f5735c7
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-25 19:59:43 +0900

    used model form to associate it with model

--- Stack_project/Stack/forms.py
+++ Stack_project/Stack/forms.py
@@ -1,8 +1,11 @@
-from django import forms
+from django.forms import ModelForm
+from .models import TaskManager
 
 
-class TaskForm(forms.Form):
-    name = forms.CharField(label="Enter the name of the task: ")
-    detail = forms.CharField(
-        label="Enter the details of your task: ", widget=forms.Textarea
-    )
+class TaskForm(ModelForm):
+    class Meta:
+        model = TaskManager
+        fields = ["task_name", "detail"]
+
+
+form = TaskForm()
