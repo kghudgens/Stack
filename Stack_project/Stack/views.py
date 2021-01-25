8cc9cf717ea8fcfcb42f6fc11d60ab2ffa4742f5
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-25 20:00:09 +0900

    added form save

--- Stack_project/Stack/views.py
+++ Stack_project/Stack/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import TaskForm
@@ -8,8 +8,9 @@ def index(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
+            form.save()
             messages.success(request, "Your task has been added")
-            return HttpResponseRedirect("/index/")
+            return redirect("index")
     else:
         form = TaskForm()
     return render(request, "Stack/index.html", {"form": form})
