7033cffec34f19e9017042037ccf048e32ffdc7b
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-25 19:59:56 +0900

    added meta

--- Stack_project/Stack/models.py
+++ Stack_project/Stack/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class TaskManager(models.Model):
     task_name = models.CharField(max_length=30)
     detail = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_name
