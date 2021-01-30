eebc05641a0c0d2e0fb9348f93b70649ac6442df
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-30 19:57:27 +0900

    created functionality for popup window

--- Stack_project/Stack/static/Stack/script.js
+++ Stack_project/Stack/static/Stack/script.js
@@ -1,3 +1,17 @@
+// Create overlay variable 
+const overlay = document.querySelector('.overlay')
+
+// hide the overlay container
+overlay.classList.add('hidden')
+
+// create function that will remove the hidden class
+function showOverlay(){
+    overlay.classList.remove('hidden')
+}
+// Set timer for window pop
+window.setTimeout(showOverlay, 10000)
+
+// Takes the user input
 document.querySelector('.save').addEventListener('click', function(){
     let formInput = document.querySelector('.formInput').textContent
     console.log(formInput);
