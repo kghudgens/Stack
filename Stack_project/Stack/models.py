d0a72b2c5ea72368c19fb45d6b853709d0cde121
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-31 15:50:57 +0900

    added author to models

--- Stack_project/Stack/models.py
+++ Stack_project/Stack/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class TaskManager(models.Model):
     task_name = models.CharField(max_length=30)
     detail = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.task_name
