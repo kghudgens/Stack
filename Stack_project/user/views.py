d92052f8f4a428056eb2db1e58dd48c2ad881767
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-31 22:23:31 +0900

    accounts are now registered

--- Stack_project/user/views.py
+++ Stack_project/user/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm()
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, "Your account has been created {username}")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
     return render(request, "user/register.html", {"form": form})
