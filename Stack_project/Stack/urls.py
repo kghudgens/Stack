a15716820acf34ccb121329026bea2815c0e5178
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-02 23:04:50 +0900

    added debug settings to handle images

--- Stack_project/Stack/urls.py
+++ Stack_project/Stack/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,3 +8,6 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("about/", views.about, name="about"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
