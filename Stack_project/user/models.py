2b148978be4e113ec177a6bd97944ef09b3c142f
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-03 10:24:27 +0900

    new fields for profile model

--- Stack_project/user/models.py
+++ Stack_project/user/models.py
@@ -4,6 +4,9 @@ from PIL import Image
 
 
 class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    tagline = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/")
 
