154b00a133eee8d27dd90f8106488fd3b7264f7b
Kevin Hudgens <hudgens1073@gmail.com>
2021-02-03 10:13:29 +0900

    added fields to form

--- Stack_project/user/forms.py
+++ Stack_project/user/forms.py
@@ -3,6 +3,10 @@ from .models import *
 
 
 class ProfileUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(label="Enter your first name", max_length=30)
+    last_name = forms.CharField(label="Enter your last name", max_length=30)
+    tagline = forms.CharField(label="Enter a tagline", max_length=100)
+
     class Meta:
         model = Profile
-        fields = ["image"]
+        fields = ["first_name", "last_name", "tagline", "image"]
